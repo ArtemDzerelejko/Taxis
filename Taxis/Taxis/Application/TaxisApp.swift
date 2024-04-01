ac844f92de6fa89a92960562839660a3ea8249f6
A <a@gmail.como>
2024-04-01 14:37:22 +0300

    Test

---
+++ Taxis/Taxis/Application/TaxisApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaxisApp.swift
+//  Taxis
+//
+//  Created by artem on 01.04.2024.
+//
+
+import SwiftUI
+
+@main
+struct TaxisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
